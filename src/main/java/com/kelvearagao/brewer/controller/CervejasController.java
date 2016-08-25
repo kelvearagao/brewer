33f7f9419d64cd925346feb539d0d8f5663b6e10
Kelve <kelvearagao@gmail.com>
2016-08-25 15:40:08 -0300

    configurando o ViewreRoulver com Thymeleaf

--- src/main/java/com/kelvearagao/brewer/controller/CervejasController.java
+++ src/main/java/com/kelvearagao/brewer/controller/CervejasController.java
@@ -1,8 +1,18 @@
 package com.kelvearagao.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
+	
+	/**
+	 * Retorna a tela de cadastro da cerveja.   
+	 *
+	 */
+	@RequestMapping("/cervejas/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
 
 }
