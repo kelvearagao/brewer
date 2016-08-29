7a72bec29383ad3071155936b5a3bf01a54c8f32
Kelve <kelvearagao@gmail.com>
2016-08-29 15:41:37 -0300

    começando cadastro de cerveja

--- src/main/java/com/kelvearagao/brewer/controller/CervejasController.java
+++ src/main/java/com/kelvearagao/brewer/controller/CervejasController.java
@@ -1,7 +1,11 @@
 package com.kelvearagao.brewer.controller;
 
+import org.omg.CORBA.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kelvearagao.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
@@ -12,6 +16,13 @@ public class CervejasController {
 	 */
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println("Cadastrar " + cerveja.getSku());
+		
 		return "cerveja/CadastroCerveja";
 	}
 
