a7a623ae93dbad5b19bbab50f58f8281dd0e2697
Kelve <kelvearagao@gmail.com>
2016-09-29 23:18:59 -0300

    Migrando o banco de dados com Flyway

--- src/main/java/com/kelvearagao/brewer/model/Estilo.java
+++ src/main/java/com/kelvearagao/brewer/model/Estilo.java
@@ -1,11 +1,13 @@
 package com.kelvearagao.brewer.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +21,9 @@ public class Estilo implements Serializable {
 	private Long codigo;
 	
 	private String nome;
+	
+	@OneToMany(mappedBy = "estilo")
+	private List<Cerveja> cervejas;
 
 	public Long getCodigo() {
 		return codigo;
